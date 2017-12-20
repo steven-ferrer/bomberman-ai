824c52863361e43670dbf6ac1729103ea08dce0c
ivanarnieranario <ivanarnieranario@outlook.com>
2017-12-20 11:30:22 -0300

    Added Grid and Nodes for Game World Point (using Gizmos to visualize debugging)

---
+++ Bomberman Starter Project/Assets/Scripts/Algorithms/GridScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridScript : MonoBehaviour {
+
+	public Transform player;
+	public LayerMask unwalkableMask;
+	public Vector2 gridWorldSize;
+	public float nodeRaduis;
+
+	Node[,] grid;
+	float nodeDiameter;
+	int gridSizeX,gridSizeY;
+
+	void Start(){
+		nodeDiameter = nodeRaduis * 2;
+		gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+		gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+		CreateGrid ();
+	}
+
+	void CreateGrid(){
+		grid = new Node[gridSizeX,gridSizeY];
+		Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+
+		for (int x = 0; x < gridSizeX; x++) {
+			for (int y = 0; y < gridSizeY; y++) {
+				Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRaduis) + Vector3.forward * (y * nodeDiameter + nodeRaduis);
+				bool walkable = !(Physics.CheckSphere (worldPoint, nodeRaduis,unwalkableMask));
+				grid [x, y] = new Node (walkable, worldPoint);
+			}
+		}
+	}
+
+	void OnDrawGizmos(){
+		Gizmos.DrawWireCube (transform.position, new Vector3 (gridWorldSize.x, 1, gridWorldSize.y));
+
+		if (grid != null) {
+			Node playerNode = NodeFromWorldPoint (player.position);
+			foreach (Node n in grid) {
+				Gizmos.color = (n.walkable) ? Color.white : Color.red;
+				if (n == playerNode) {
+					Gizmos.color = Color.cyan;
+				}
+				Gizmos.DrawCube (n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+			}
+		}
+	}
+
+	public Node NodeFromWorldPoint(Vector3 worldPosition){
+		float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+		float percentY = (worldPosition.z + gridWorldSize.y / 2) / gridWorldSize.y;
+		percentX = Mathf.Clamp01 (percentX);
+		percentY = Mathf.Clamp01 (percentY);
+
+		int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+		int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+
+		return grid [x, y];
+	}
+}
